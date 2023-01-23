08d1aa19b6f4a01b91581039115921d7b404ec0c
chris <5sensprod@gmail.com>
2023-01-23 08:01:13 +0100

    Implémentation form + valdation input

--- modal.js
+++ modal.js
@@ -35,5 +35,93 @@ function closeModal(e) {
   }
 }
 
-//validate input
+const form = document.querySelector("form");
+const firstName = document.querySelector("#first");
+const lastName = document.querySelector("#last");
+const email = document.querySelector("#email");
+const birthdate = document.querySelector("#birthdate");
+const number = document.querySelector("#number");
+const radios = document.querySelectorAll("input[type=radio]");
+const checkbox = document.querySelector("#checkbox");
+const submitBtn = document.querySelector("input[type=submit]");
 
+form.addEventListener("submit", validateForm);
+
+function validateForm(e) {
+  e.preventDefault();
+  let isValid = true;
+
+  // validate first name
+  if (firstName.value.length < 2 || firstName.value === "") {
+    firstName.nextElementSibling.innerHTML = "Minimum 2 characters";
+    isValid = false;
+  } else {
+    firstName.nextElementSibling.innerHTML = "";
+  }
+
+  // validate last name
+  if (lastName.value.length < 2 || lastName.value === "") {
+    lastName.nextElementSibling.innerHTML = "Minimum 2 characters";
+    isValid = false;
+  } else {
+    lastName.nextElementSibling.innerHTML = "";
+  }
+
+// validate email
+const email = document.getElementById("email");
+const emailRegEx = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+if (!emailRegEx.test(email.value)) {
+    email.nextElementSibling.innerHTML = "Invalid email";
+    isValid = false;
+} else {
+    email.nextElementSibling.innerHTML = "";
+}
+
+// validate birthdate
+
+const birthdate = document.getElementById("birthdate");
+if(!birthdate.value) {
+  birthdate.nextElementSibling.innerHTML = "Vous devez entrer votre date de naissance.";
+  isValid = false;
+} else {
+    birthdate.nextElementSibling.innerHTML = "";
+}
+
+  // validate number
+  if (isNaN(number.value)) {
+    numberContest.nextElementSibling.innerHTML = "Please enter a number";
+    isValid = false;
+  } else {
+    number.nextElementSibling.innerHTML = "";
+  }
+
+  // validate radio buttons
+  let radioChecked = false;
+  radios.forEach((radio) => {
+    if (radio.checked) {
+      radioChecked = true;
+    }
+  });
+  if (!radioChecked) {
+    document.querySelector(".radio-error").innerHTML = "Please select an option";
+    isValid = false;
+  } else {
+    document.querySelector(".radio-error").innerHTML = "";
+  }
+
+  const termsCheckbox = document.getElementById("checkbox1");
+  if (!termsCheckbox.checked) {
+    termsCheckbox.nextElementSibling.innerHTML = "Vous devez accepter les conditions d'utilisation";
+    isValid = false;
+  } else {
+    termsCheckbox.nextElementSibling.innerHTML = "";
+  }
+
+  // save form data if valid
+  if (isValid) {
+    // save form data
+    // ...
+    // clear form
+    form.reset();
+  }
+}
